31ebe30f881f47833263b44a8731e098cdce6b8d
Joseph Smith <joseph.smit95@gmail.com>
2023-11-18 16:34:33 -0500

    First Commit

---
+++ ruby_arrays1.rb
@@ -0,0 +1,24 @@
+# test_1.rb
+
+# Question 1: Return the sum of all elements in the array.
+def sum_of_elements(arr)
+    # Your code here
+  end
+  
+  # Question 2: Find and return the maximum value in the array.
+  def find_max_value(arr)
+    # Your code here
+  end
+  
+  # Question 3: Return a new array with only the unique elements from the original array.
+  def unique_elements(arr)
+    # Your code here
+  end
+  
+  # Test the methods with sample data
+  sample_array = [1, 2, 3, 4, 5]
+  
+  puts "1. Sum of elements: #{sum_of_elements(sample_array)}"
+  puts "2. Max value: #{find_max_value(sample_array)}"
+  puts "3. Unique elements: #{unique_elements(sample_array)}"
+  
---
+++ ruby_arrays2.rb
@@ -0,0 +1,30 @@
+# test_2.rb
+
+# Question 1: Return the sum of all elements in the array.
+def sum_of_elements(arr)
+    # Your code here
+  end
+  
+  # Question 2: Find and return the maximum value in the array.
+  def find_max_value(arr)
+    # Your code here
+  end
+  
+  # Question 3: Return a new array with only the unique elements from the original array.
+  def unique_elements(arr)
+    # Your code here
+  end
+  
+  # Question 4: Check if the array includes a specific element.
+  def includes_element?(arr, element)
+    # Your code here
+  end
+  
+  # Test the methods with sample data
+  sample_array = [1, 2, 3, 4, 5, 6, 7]
+  
+  puts "1. Sum of elements: #{sum_of_elements(sample_array)}"
+  puts "2. Max value: #{find_max_value(sample_array)}"
+  puts "3. Unique elements: #{unique_elements(sample_array)}"
+  puts "4. Includes element 3? #{includes_element?(sample_array, 3)}"
+  
