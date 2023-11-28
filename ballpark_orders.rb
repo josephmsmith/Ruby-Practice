200de51e05cf3b7851769932e354cc7e4702decb
Joseph Smith <joseph.smit95@gmail.com>
2023-11-27 22:13:11 -0500

    adding a repeat method for learning purposes

--- ballpark_orders.rb
+++ ballpark_orders.rb
@@ -27,3 +27,41 @@ def order_calculation(order)
 end
 
 order_calculation(order)
+
+
+
+
+# repeat except applying principle of classes 
+
+=begin
+determine cost of 4 items, if order = else then order a coke
+input = string delimited by spaces
+output = total amount of order
+
+=end
+
+MENUS = {pizza:5.00, coke:4.00, popcorn:2.50, smoothie: 7}
+print "What would you like to order today?: "
+new_order = gets.chomp.split(" ")
+
+def ord_calc(new_order)
+    total_cost = 0
+    new_order.each do |item|
+        sym = item.to_sym
+        if MENUS.has_key?(sym)
+            cost = MENUS[sym]
+            total_cost +=cost
+        else
+            cost = MENUS[:coke]
+            total_cost += cost
+        end
+    end
+    total_cost = (total_cost + total_cost * TAX).round(2)
+    puts "The total cost of your order is #{total_cost}"
+end
+
+ord_calc(new_order)
+    
+
+
+
