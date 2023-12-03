3bc1a526fb8ac76440c425c76bdf541265e18805
Joseph Smith <joseph.smit95@gmail.com>
2023-12-03 14:15:16 -0500

    added helper method logic and SOLID principle

--- snapcracklepop.rb
+++ snapcracklepop.rb
@@ -12,20 +12,55 @@ output: 6 sounds on one line deliminated by spaces
 
 =end
 
-def snapcracklepop
-    sounds_array = []
-    6.times do |number|
-        print "How many rice krispies are in the bowl? : "
-        number = gets.chomp.to_i
-        if number %3 == 0
-            sound = "pop"
-        elsif number.even?
-            sound = "snap"
-        else sound = "crackle"
-        end
-        sounds_array << sound
+# ORIGINAL VERSION
+# def snapcracklepop
+#     sounds_array = []
+#     6.times do |number|
+#         print "How many rice krispies are in the bowl? : "
+#         number = gets.chomp.to_i
+#         if number %3 == 0
+#             sound = "pop"
+#         elsif number.even?
+#             sound = "snap"
+#         else sound = "crackle"
+#         end
+#         sounds_array << sound
+#     end
+#     puts sounds_array.join(" ")
+# end
+
+# new version
+
+def determine_sound(number)
+    case
+    when number % 3 == 0
+      "pop"
+    when number.even?
+      "snap"
+    else
+      "crackle"
     end
+  end
+  
+def snapcracklepop(times = 6)
+    sounds_array = []
+  
+    times.times do |index|
+      print "How many rice krispies are in bowl number #{index + 1}? : "
+      input = gets.chomp
+  
+      # Check if input is a number
+      if input =~ /^\d+$/
+        number = input.to_i
+        sounds_array << determine_sound(number)
+      else
+        puts "Please enter a valid number."
+        redo
+      end
+    end
+
     puts sounds_array.join(" ")
 end
-
+  
 snapcracklepop
+  
