a92d2be36c5a83748800aae86156bb80e4b558bb
Joseph Smith <joseph.smit95@gmail.com>
2023-12-09 13:57:54 -0500

    added log level method

--- log_line_parser.rb
+++ log_line_parser.rb
@@ -1,21 +1,26 @@
 #!/usr/bin/env ruby
 
 class LogLineParser
-    def initialize(line)
-      @line = line
-    end
-  
-    def message
-      @line.gsub(/\[\w+\]:\s*/, '').strip
-    end
-  
-    def log_level
-      raise 'Please implement the LogLineParser#log_level method'
-    end
-  
-    def reformat
-      raise 'Please implement the LogLineParser#reformat method'
-    end
+  def initialize(line)
+    @line = line
   end
 
-  LogLineParser.new("[WARNING]: \tTimezone not set \r\n")
+  def message
+    @line.gsub(/\[\w+\]:\s*/, '').strip
+  end
+
+  def log_level
+    t1 = @line.index("[")+1
+    t2 = @line.index("]")-1
+    @line[t1,t2].downcase
+  end
+
+  def reformat
+    raise 'Please implement the LogLineParser#reformat method'
+  end
+end
+
+# testing 
+puts LogLineParser.new("[WARNING]: \tTimezone not set \r\n").message
+puts LogLineParser.new('[ERROR]: Disk full').log_level
+puts LogLineParser.new('[WARNING]: Unsafe password').log_level
