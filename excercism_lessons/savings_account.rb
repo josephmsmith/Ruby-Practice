cc0b52c78b95da00efbea4070422318861a0fa41
Joseph Smith <joseph.smit95@gmail.com>
2023-12-12 09:05:08 -0500

    started savings_account.rb

---
+++ excercism_lessons/savings_account.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+=begin
+1.Implement the SavingsAccount.interest_rate method to calculate the interest rate based on the specified balance:
+test using SavingsAccount.interest_rate(200.75)
+
+
+=end
+
+module SavingsAccount
+    def interest_rate(balance)
+    end
+end
+
+puts SavingsAccount.interest_rate(200.75)
