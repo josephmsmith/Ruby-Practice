e1a8743d2b51ddf30c93f47e9deb7a0ea41e09c9
Joseph Smith <joseph.smit95@gmail.com>
2023-12-12 20:51:11 -0500

    final commit 12/12

--- excercism_lessons/savings_account.rb
+++ excercism_lessons/savings_account.rb
@@ -2,36 +2,42 @@
 
 =begin
 In this exercise you will be working with savings accounts. 
-Each year, the balance of a savings account is updated based on the interest rate. 
-The interest rate the bank gives depends on the amount of money in the accounts (its balance):
-
-0.5% for a non-negative balance less than 1000 dollars.
-1.621% for a positive balance greater than or equal to 1000 dollars and less than 5000 dollars.
-2.475% for a positive balance greater than or equal to 5000 dollars.
-3.213% for a negative balance (results in negative interest).
+    Each year, the balance of a savings account is updated based on the interest rate. 
+    The interest rate the bank gives depends on the amount of money in the accounts (its balance):
+        0.5% for a non-negative balance less than 1000 dollars.
+        1.621% for a positive balance greater than or equal to 1000 dollars and less than 5000 dollars.
+        2.475% for a positive balance greater than or equal to 5000 dollars.
+        3.213% for a negative balance (results in negative interest).
 
 1.Implement the SavingsAccount.interest_rate method to calculate the interest rate based on the specified balance:
-test using SavingsAccount.interest_rate(200.75)
-
+    test using SavingsAccount.interest_rate(200.75)
+2. Implement the SavingsAccount.annual_balance_update method to calculate the annual balance update, 
+    taking into account the interest rate:
 
 =end
 # turn into float
 module SavingsAccount
+    # calc interest_rate based on balance, initiate using self 
     def self.interest_rate(balance)
         if (0..1000).include?(balance)
             0.5
         elsif (1000..5000).include?(balance)
-            0.01621
+            1.621
         elsif balance >= 5000
-            0.02475
+            2.475
         else
-            -0.03213
+            -3.213
         end
+    end
+    # calc balance using interest rate 
+    def self.annual_balance_update(balance)
+        balance + (interest_rate(balance) * balance)
     end
 end
 
-puts SavingsAccount.interest_rate(200.75)
+puts SavingsAccount.interest_rate(200)
 # test
 # puts SavingsAccount.interest_rate(1007)
 # puts SavingsAccount.interest_rate(5045)
 # puts SavingsAccount.interest_rate(-34)
+puts SavingsAccount.annual_balance_update(200)
