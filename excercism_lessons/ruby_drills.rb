0231c8b2ae741190df4c1e9ed4370e3b2b10e583
Joseph Smith <joseph.smit95@gmail.com>
2023-12-23 12:11:31 -0500

    quick edit to shortest array method

--- excercism_lessons/ruby_drills.rb
+++ excercism_lessons/ruby_drills.rb
@@ -1,34 +1,34 @@
 #!/usr/bin/env ruby
 
-# # String Interpolation
+# String Interpolation
 
-# color = 'green'
-# turtle = 'Leonardo'
-# weapon = 'swords'
-# star = 'Joseph Gordon Levitt'
-# movie = 'inception'
+color = 'green'
+turtle = 'Leonardo'
+weapon = 'swords'
+star = 'Joseph Gordon Levitt'
+movie = 'inception'
 
-# puts "My favorite color is #{color}. \nMy favorite ninja turtle is #{turtle}. His weapon is #{weapon}. 
-# #{star} is a great movie star. My favorite movie of theirs is #{movie}."
+puts "My favorite color is #{color}. \nMy favorite ninja turtle is #{turtle}. His weapon is #{weapon}. 
+#{star} is a great movie star. My favorite movie of theirs is #{movie}."
 
-# # Fizzbuzz refresher
+# Fizzbuzz refresher
 
-# # number divisible by 5 print "Fizz"
-# # If the number is divisible by three print out "Buzz"
-# # If the number is divisible by both five and three print out "Fizz Buzz"
-# # If the number is not divisible by either five or three print out "(num) is not fizzable"
+# number divisible by 5 print "Fizz"
+# If the number is divisible by three print out "Buzz"
+# If the number is divisible by both five and three print out "Fizz Buzz"
+# If the number is not divisible by either five or three print out "(num) is not fizzable"
 
-# 1.upto(20) do |i|
-#     if i % 5 == 0 && i % 3 == 0 
-#         puts "FizzBuzz"
-#     elsif i % 5 == 0 
-#         puts "Fizz"
-#     elsif i % 3 == 0 
-#         puts "Buzz"
-#     else
-#         puts "#{i} is not fizzable"
-#     end
-# end
+1.upto(20) do |i|
+    if i % 5 == 0 && i % 3 == 0 
+        puts "FizzBuzz"
+    elsif i % 5 == 0 
+        puts "Fizz"
+    elsif i % 3 == 0 
+        puts "Buzz"
+    else
+        puts "#{i} is not fizzable"
+    end
+end
 
 # Nested Arrays
 
@@ -36,7 +36,7 @@ arr = [["Bobby", "Jaden", "Denis"], ["Red", "Blue", "Green"], ["Limp Bizkit", "3
 
 
 # Find the length of the shortest array
-min_length = arr.map(&:length).min
+min_length = arr.map{|item| item.length ).min
 
 # Iterate through each index of the nested arrays up to the shortest array's length
 min_length.times do |i|
